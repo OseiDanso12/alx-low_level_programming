fc3601013bb59f01cd844bd38e5943b6f5cbec00
Osei Danso12 <melchizedekosei0@gmail.com>
2023-02-16 06:56:01 -0800

    5-printf.c update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio>
+#include <stdio.h>
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art" followed by a new line
- * Return: Always 0.
+ * main - A program that print a line using the printf command
+ * Return: 0(Success)
  */
 int main(void)
 {
-printf(with proper grammar, but the outcome is a piece of art,/n");
-return (0);
+	prinftf("with proper grammar, but the outcome is a piece of art/n");
+	return (0);
 }
