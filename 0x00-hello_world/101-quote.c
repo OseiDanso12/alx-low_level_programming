0a2f35c75df4eab4ae190ca2e7a290d93e40e81d
Osei Danso12 <melchizedekosei0@gmail.com>
2023-02-16 12:05:37 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - A C program that prints a line to the standard error
- * Return 1 (Success)
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * followed by a newline, to standard error.
+ * Return: 1 (success)
  */
-int main(void)
+int mai(void)
 {
-	fput(and that piece of art is useful\" - Dora Korpar, 2015-10-19/n", stdout);
+	write(and that piece of art is useful\" - Dora Korpar, 2015-10-19);
 	return (1);
 }
