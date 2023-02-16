fdaf443abee31797ac5688bf2c7b21bd8d1a7c74
Osei Danso12 <melchizedekosei0@gmail.com>
2023-02-16 05:14:37 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+
+
 /** 
  * main -Aprogram that prints a line with puts function
  * Return: 0 (success)
  */
+
+
 int main(void)
+
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
+
